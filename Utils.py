561bd71a35276378d81e6517193e8b21987021c6
ferru97 <vitof970@gmail.com>
2020-12-23 16:15:28 +0100

    Fix

--- Utils.py
+++ Utils.py
@@ -2,7 +2,7 @@ dataset_folder = "dataset/"
 results_folder = "results/"
 
 
-def text4csv(test):
+def text4csv(text):
     text = str(text)
     text = text.replace(";"," ")
     text = text.replace("\n"," ")
